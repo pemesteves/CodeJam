1804ddf621719ca53778e80e3e8a75ad16844954
Pedro Esteves <up201705160@fe.up.pt>
2019-05-18 10:50:27 +0100

    Changed solution of one exercise

--- CodeJam2019/Qualification Round/you_can_go_your_own_way.cpp
+++ CodeJam2019/Qualification Round/you_can_go_your_own_way.cpp
@@ -10,65 +10,14 @@ using namespace std;
 
 vector<string> results;
 
-bool insert_character(vector<vector<char>> &v, string &s, unsigned i, unsigned j){
-    if(i == v.size()-1 && j == v.size()-1)
-        return true;
-    if(i >= v.size() || j >= v.size())
-        return false;
-    
-    if(v[i][j] == '\0'){
-        v[i][j] = 'S';
-        s.push_back('S');
-        if(insert_character(v, s, i+1, j))
-            return true;
-        s.pop_back();
-        v[i][j] = '\0';
-        v[i][j] = 'E';
-        s.push_back('E');
-        if(insert_character(v, s, i, j+1))
-            return true;
-        s.pop_back();
-        return false;
-    }
-    
-    if(v[i][j] == 'e'){
-        v[i][j] = 'S';
-        s.push_back('S');
-        if(insert_character(v, s, i+1, j))
-            return true;
-        s.pop_back();
-        v[i][j] = 'e';
-        return false;
-    }
-    
-    if(v[i][j] == 's'){
-        v[i][j] = 'E';
-        s.push_back('E');
-        if(insert_character(v, s, i, j+1))
-            return true;
-        s.pop_back();
-        v[i][j] = 's';
-        return false;
-    }
-    
-    return false;
-}
-    
-
 void func_threads(unsigned i, unsigned N, string P){
-    vector<vector<char>> v(N, vector<char>(N, '\0'));
-    int j = 0, k = 0;
-    for(unsigned i = 0; i < 2*N-2; i++){
-        v[j][k] = tolower(P[i]);
-        if(P[i] == 'E')
-            k++;
-        else if(P[i] == 'S')
-            j++;
-    }
-    
     string result;
-    insert_character(v, result, 0, 0);
-    
+    for(unsigned j = 0; j < P.length(); j++){
+        if(P[j] == 'E')
+            result.push_back('S');
+        else
+            result.push_back('E');
+    }
     results[i] = result;
 }
 
